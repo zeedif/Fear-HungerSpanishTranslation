571d4072aecf6734158ea2799206f6845e544eed
Zeedif <carlos_antonio-rl@hotmail.com>
2023-07-06 21:24:02 -0600

    Create JSON mapping classes for System.json

---
+++ lib/models/DataSystem/ship.dart
@@ -0,0 +1,26 @@
+import 'package:FearHungerTranslation/models/DataSystem/audio.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'ship.g.dart';
+
+@JsonSerializable()
+class Ship {
+  Audio bgm;
+  int characterIndex;
+  String characterName;
+  int startMapId;
+  int startX;
+  int startY;
+
+  Ship({
+    required this.bgm,
+    required this.characterIndex,
+    required this.characterName,
+    required this.startMapId,
+    required this.startX,
+    required this.startY,
+  });
+
+  factory Ship.fromJson(Map<String, dynamic> json) => _$ShipFromJson(json);
+  Map<String, dynamic> toJson() => _$ShipToJson(this);
+}
---
+++ lib/models/data_system.dart
@@ -0,0 +1,117 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'DataSystem/attack_motions.dart';
+import 'DataSystem/audio.dart';
+import 'DataSystem/terms.dart';
+import 'DataSystem/test_battlers.dart';
+import 'DataSystem/ship.dart';
+
+part 'data_system.g.dart';
+
+@JsonSerializable()
+class DataSystem {
+  Ship airship;
+  List<String> armorTypes;
+  List<AttackMotion> attackMotions;
+  Audio battleBgm;
+  String battleback1Name;
+  String battleback2Name;
+  int battlerHue;
+  String battlerName;
+  Ship boat;
+  String currencyUnit;
+  Audio defeatMe;
+  int editMapId;
+  List<String> elements;
+  List<String> equipTypes;
+  String gameTitle;
+  Audio gameoverMe;
+  String locale;
+  List<int> magicSkills;
+  List<bool> menuCommands;
+  bool optDisplayTp;
+  bool optDrawTitle;
+  bool optExtraExp;
+  bool optFloorDeath;
+  bool optFollowers;
+  bool optSideView;
+  bool optSlipDeath;
+  bool optTransparent;
+  List<int> partyMembers;
+  Ship ship;
+  List<String> skillTypes;
+  List<Audio> sounds;
+  int startMapId;
+  int startX;
+  int startY;
+  List<String> switches;
+  Terms terms;
+  List<TestBattlers> testBattlers;
+  int testTroopId;
+  String title1Name;
+  String title2Name;
+  Audio titleBgm;
+  List<String> variables;
+  int versionId;
+  Audio victoryMe;
+  List<String> weaponTypes;
+  List<int> windowTone;
+  bool hasEncryptedImages;
+  bool hasEncryptedAudio;
+  String encryptionKey;
+
+  DataSystem({
+    required this.airship,
+    required this.armorTypes,
+    required this.attackMotions,
+    required this.battleBgm,
+    required this.battleback1Name,
+    required this.battleback2Name,
+    required this.battlerHue,
+    required this.battlerName,
+    required this.boat,
+    required this.currencyUnit,
+    required this.defeatMe,
+    required this.editMapId,
+    required this.elements,
+    required this.equipTypes,
+    required this.gameTitle,
+    required this.gameoverMe,
+    required this.locale,
+    required this.magicSkills,
+    required this.menuCommands,
+    required this.optDisplayTp,
+    required this.optDrawTitle,
+    required this.optExtraExp,
+    required this.optFloorDeath,
+    required this.optFollowers,
+    required this.optSideView,
+    required this.optSlipDeath,
+    required this.optTransparent,
+    required this.partyMembers,
+    required this.ship,
+    required this.skillTypes,
+    required this.sounds,
+    required this.startMapId,
+    required this.startX,
+    required this.startY,
+    required this.switches,
+    required this.terms,
+    required this.testBattlers,
+    required this.testTroopId,
+    required this.title1Name,
+    required this.title2Name,
+    required this.titleBgm,
+    required this.variables,
+    required this.versionId,
+    required this.victoryMe,
+    required this.weaponTypes,
+    required this.windowTone,
+    required this.hasEncryptedImages,
+    required this.hasEncryptedAudio,
+    required this.encryptionKey,
+  });
+
+  factory DataSystem.fromJson(Map<String, dynamic> json) => _$DataSystemFromJson(json);
+  Map<String, dynamic> toJson() => _$DataSystemToJson(this);
+}
